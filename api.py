5765fbc30dba7cef8937c95e241b6780e37c37b3
Melissa Albarella <moexu13@gmail.com>
2025-03-10 13:18:27 -0400

    Send code snippet for AI analysis

--- api.py
+++ api.py
@@ -1,20 +1,31 @@
+# flask --app api.py --debug run
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain.chat_models import init_chat_model
+
+model = init_chat_model(os.getenv('ANTHROPIC_MODEL'), model_provider='anthropic')
+prompt = SystemMessage("You are a senior software engineer. Give feedback on this code to help a junior engineer.")
+
 from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-todos = {}
+class CodeAnalysis(Resource):
+    def get(self):
+        return { 'message': 'Code analysis API v1' }
+    
+    def post(self):
+        data = request.get_json()
+        code = data['data']['code']
+        output = model.invoke([prompt, HumanMessage(code)])
+        return { 'code': output.content }, 201
 
-class TodoSimple(Resource):
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
-
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
-
-api.add_resource(TodoSimple, '/<string:todo_id>')
+api.add_resource(CodeAnalysis, '/v1/code')
 
 if __name__ == '__main__':
     app.run(debug=True)
