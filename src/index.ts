bbd00d38d6c0fe595010bda57a7052867417b95e
Melissa Albarella <moexu13@gmail.com>
2025-03-31 12:33:40 -0400

    Split routes into separate files

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { Hono } from "hono";
 import { handle } from "hono/aws-lambda";
+import gh_app from "./gh/gh.routes";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+app.route("/gh", gh_app);
 
 if (import.meta.env.DEV) {
   console.log('🔥 Dev server started');
