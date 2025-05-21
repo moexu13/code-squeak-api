68b0ca948899122839c71b0c9f2c74bedb43a8a8
Melissa Albarella <moexu13@gmail.com>
2025-05-21 15:43:59 -0400

    Clean up error handler

--- src/errors/errorHandler.ts
+++ src/errors/errorHandler.ts
@@ -4,23 +4,31 @@
 import { Request, Response, NextFunction } from "express";
 import logger from "../utils/logger";
 import { config } from "../config/env";
+import { createSanitizedError } from "../utils/errorUtils";
 
 export default function errorHandler(
   err: Error & { status?: number },
-  _req: Request,
+  req: Request,
   res: Response,
   _next: NextFunction
 ) {
-  // Log the error
+  // Create sanitized error object
+  const sanitizedError = createSanitizedError(err, req);
+
+  // Log the sanitized error
   logger.error({
     message: "Express error occurred",
-    error: err,
+    error: sanitizedError,
   });
 
   // Only report to Sentry if not in test environment
   if (!config.env.isTest) {
     import("@sentry/node").then((Sentry) => {
-      Sentry.captureException(err);
+      Sentry.captureException(err, {
+        extra: {
+          request: sanitizedError.request,
+        },
+      });
     });
   }
 
@@ -30,6 +38,6 @@ export default function errorHandler(
   // Handle errors
   res.status(status).json({
     error: "Something went wrong",
-    message: config.env.isDevelopment ? err.message : undefined,
+    message: config.env.isDevelopment ? sanitizedError.message : undefined,
   });
 }
